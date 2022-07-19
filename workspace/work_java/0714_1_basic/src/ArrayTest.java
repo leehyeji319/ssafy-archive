ca5ed7928463ab34a446d0ca4801a1103a6f6648
=leehyeji <=zzambbang@kakao.com>
2022-07-19 10:21:52 +0900

    modify ArrayTest.java print length

--- workspace/work_java/0714_1_basic/src/ArrayTest.java
+++ workspace/work_java/0714_1_basic/src/ArrayTest.java
@@ -2,7 +2,10 @@
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int[] arr = {1,2,3};
+		System.out.println(arr.length);
 
 	}
 
