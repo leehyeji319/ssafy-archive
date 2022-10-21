7b3a07132347635b146de0b11d1a0465141635df
modsiw <zzambbang@gmail.com>
2022-10-21 17:38:56 +0900

    1021 spring mybatis

--- step05_mvc_mybatis2_noimpl/src/main/java/com/ssafy/empapp/model/dao/DeptDAO.java
+++ step05_mvc_mybatis2_noimpl/src/main/java/com/ssafy/empapp/model/dao/DeptDAO.java
@@ -2,11 +2,12 @@ package com.ssafy.empapp.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.empapp.model.dto.Dept;
 
-@Repository
+@Mapper
 public interface DeptDAO {
 
 	int insertDept(Dept dept);
